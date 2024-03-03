7366583683dd7a97dfa68260fdbfeb087dcb53e9
jpkamdem <jpkamdem@proton.me>
2024-03-03 16:19:55 +0100

    routing fini, problème image en app component présent sur toutes les pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CarteComponent } from './views/carte/carte.component';
+import { InfosComponent } from './views/infos/infos.component';
+import { PanierComponent } from './views/panier/panier.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'carte', component: CarteComponent},
+    {path: 'infos', component: InfosComponent},
+    {path: 'panier' , component: PanierComponent}
+];
