311476de7e6eaea7334d173fc6289d6bc82fda74
jpkamdem <jpkamdem@proton.me>
2024-03-04 23:01:55 +0100

    not found ajouté

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { CarteComponent } from './views/carte/carte.component';
 import { InfosComponent } from './views/infos/infos.component';
 import { PanierComponent } from './views/panier/panier.component';
 import { AccueilComponent } from './views/accueil/accueil.component';
+import { NotfoundComponent } from './views/notfound/notfound.component';
 
 export const routes: Routes = [
+    {path: '', component: AccueilComponent},
+    {path: 'accueil', component: AccueilComponent},
     {path: 'carte', component: CarteComponent},
     {path: 'infos', component: InfosComponent},
     {path: 'panier' , component: PanierComponent},
-    {path: 'accueil', component: AccueilComponent}
+    {path: '**', component: NotfoundComponent},
 ];
