50d79d10f36e6f38c81e6d6cf2b94a0d5e9656d6
jpkamdem <jpkamdem@proton.me>
2024-03-04 22:54:50 +0100

    accueil modifié

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { CarteComponent } from './views/carte/carte.component';
 import { InfosComponent } from './views/infos/infos.component';
 import { PanierComponent } from './views/panier/panier.component';
+import { AccueilComponent } from './views/accueil/accueil.component';
 
 export const routes: Routes = [
     {path: 'carte', component: CarteComponent},
     {path: 'infos', component: InfosComponent},
-    {path: 'panier' , component: PanierComponent}
+    {path: 'panier' , component: PanierComponent},
+    {path: 'accueil', component: AccueilComponent}
 ];
